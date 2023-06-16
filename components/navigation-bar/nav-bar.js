d8dfa5c52f0519a2f9f56b00b4cce4b2f7f527c4
Oliver <oliver.lewandowski1@gmail.com>
2023-06-16 17:21:26 +0200

    small changes

--- components/navigation-bar/nav-bar.js
+++ components/navigation-bar/nav-bar.js
@@ -1,19 +1,15 @@
-import { Fragment } from 'react';
-import styles from './nav-bar.module.css';
-import Link from 'next/link';
-import NavLogo from './nav-logo';
-import useTranslation from 'next-translate/useTranslation';
-import LoginButton from '../auth/login-button';
-import { useSession, signOut } from 'next-auth/react';
-import LogoutButton from '../auth/logout-button';
-import LanguageSwitcher from '../layout/language/language-switcher';
-import { useRouter } from 'next/router';
+import styles from "./nav-bar.module.css";
+import Link from "next/link";
+import useTranslation from "next-translate/useTranslation";
+import LoginButton from "../auth/login-button";
+import { useSession, signOut } from "next-auth/react";
+import LogoutButton from "../auth/logout-button";
+import LanguageSwitcher from "../layout/language/language-switcher";
+import { useRouter } from "next/router";
 
 const NavLink = ({ id, text, route, active }) => {
-  console.log(route, active);
-
   return (
-    <li key={id} className={active ? styles.active : ''}>
+    <li key={id} className={active ? styles.active : ""}>
       {<Link href={route}>{text}</Link>}
     </li>
   );
@@ -21,19 +17,19 @@ const NavLink = ({ id, text, route, active }) => {
 
 const NavBar = () => {
   const { data: session } = useSession();
-  const { t } = useTranslation('nav-bar');
+  const { t } = useTranslation("nav-bar");
   const router = useRouter();
 
   const unprotectedRoutes = [
-    { id: 'up-route-0', route: '/', text: t('homePage') },
-    { id: 'up-route-1', route: '/administration', text: t('administration') },
-    { id: 'up-route-2', route: '/hydroacoustic', text: t('hydroacoustic') },
-    { id: 'up-route-3', route: '/database', text: t('database') },
-    { id: 'up-route-4', route: '/contact', text: t('contact') },
+    { id: "up-route-0", route: "/", text: t("homePage") },
+    { id: "up-route-1", route: "/administration", text: t("administration") },
+    { id: "up-route-2", route: "/hydroacoustic", text: t("hydroacoustic") },
+    { id: "up-route-3", route: "/database", text: t("database") },
+    { id: "up-route-4", route: "/contact", text: t("contact") },
   ];
 
   const protectedRoutes = [
-    { id: 'p-route-1', route: '/panel', text: t('panel') },
+    { id: "p-route-1", route: "/panel", text: t("panel") },
   ];
 
   return (
@@ -48,11 +44,11 @@ const NavBar = () => {
             />
           ))}
           {session && protectedRoutes.map((props) => <NavLink {...props} />)}
-          {/* {session ? (
-            <LogoutButton signOut={signOut} key='logout-button' t={t} />
+          {session ? (
+            <LogoutButton signOut={signOut} key="logout-button" t={t} />
           ) : (
-            <LoginButton key='login-button' t={t} />
-          )} */}
+            <LoginButton key="login-button" t={t} />
+          )}
           <LanguageSwitcher />
         </ul>
       </nav>
